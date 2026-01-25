f5b67a120788504a218f6f1ee5b21f03ec108003
lggarcia <Carpediem2023->
2026-01-25 18:44:45 -0400

    Add Astro configuration and GitHub Actions workflow for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://LgPJ.github.io/core-supply/',
+  base: '/core-supply/',
+});
